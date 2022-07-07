f2ff81096f836c6deefed92a7fe0e16a209d920e
Mikebainbain <mlopez749@gmail.com>
2022-07-07 18:13:36 -0500

    Creamos el archivo variables.py en donde explicamos avriable sy comentarios en python

---
+++ variables.py
@@ -0,0 +1,20 @@
+# En Python no existe una declaracion previa del tipo de variable  al crear una 
+# Ejemplos
+var1 = 50 
+var2 = "Variable tipo string"
+datoFloat = 35845.215215
+boleano = True
+
+print(var1)
+print(var2)
+print(datoFloat)
+print(boleano)
+
+# Se pueden hacer las declaraciones en una sola linea de codigo 
+var3,var4,var5 = "Esta es la variable 3", "y Esta es la variable 4 ",50
+print(var3,var4,var5) 
+
+"""
+este es Unn comentario Largo en Python
+
+"""
