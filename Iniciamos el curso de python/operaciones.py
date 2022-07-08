3407f67872ce4c31274682679ae3fb89eb7f0d49
Mikebainbain <mlopez749@gmail.com>
2022-07-07 19:01:59 -0500

    Elaboramos el archivo operaciones donde mostramos cuales son las operaciones frecuentes

---
+++ Iniciamos el curso de python/operaciones.py
@@ -0,0 +1,13 @@
+# En este archivo mostramos las operaciones aritmeticas con el lenguaje python
+
+numero1 = 50
+numero2 = 100
+
+suma = numero1+numero2
+resta = numero1-numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+residuo = numero1%numero2
+potencia = numero1**numero2
+
+print("la suma es ",suma, "La resta es ", resta ,"La multiplicaicon es ",multiplicacion,"La division es ",division,"El residuo es ",residuo,"La potencia es ", potencia)
