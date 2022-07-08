333675630a20004670073af02bf5e6a5017059e9
Mikebainbain <mlopez749@gmail.com>
2022-07-08 16:56:57 -0500

    Matrices (Arreglos dentro de arreglos )

---
+++ Iniciamos el curso de python/matrices.py
@@ -0,0 +1,12 @@
+#Posicion 0   1
+lista1 = [50,60]
+lista2 = [70,90]
+
+print(lista1,lista2)
+
+matriz = [lista1,lista2]
+
+print ("arreglo 1 posicion 1 "+str(matriz[0][1]))
+print ("arreglo 1 posicion 0 "+str(matriz[0][0]))
+print ("arreglo 2 posicion 1 "+str(matriz[1][1]))
+print ("arreglo 2 posicion 0 "+str(matriz[1][0]))
