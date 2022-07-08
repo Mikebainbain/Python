cab4b8bf72918540e365e8e9afa3ed65478065c2
Mikebainbain <mlopez749@gmail.com>
2022-07-07 20:23:21 -0500

    Condicional if y operadores relacionales

---
+++ Iniciamos el curso de python/Condicionales.py
@@ -0,0 +1,21 @@
+# En este archivo mostramos la sintaxis de los condicionales en python
+
+from ast import If
+
+var = input("Ingresa el numero a evaluar \n")
+
+if var < 5:
+    print("No aprobaste :/ keep trying ")
+else:
+    print("!! Felicitaciones aprobaste ¡¡")
+
+
+# Operadores relacionales
+"""""
+   < menor que
+   > mayor que
+   <= menor igual que 
+   >= mayor igual que 
+   == Igual que 
+   != Diferente que 
+"""""
