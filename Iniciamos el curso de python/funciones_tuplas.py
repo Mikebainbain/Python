4260c7f783d053c190167fc9c3b6ebdafc865540
Mikebainbain <mlopezb749@gmail.com>
2022-07-10 23:59:30 -0500

    funciones par tuplas

---
+++ Iniciamos el curso de python/funciones_tuplas.py
@@ -0,0 +1,8 @@
+# archivo que muestra alguans funciones apra tuplas 
+
+tupla = (1,2,3,4,5,6,7,8,9,8,8,8,8)
+print(tupla)
+
+print(tupla.index(3)) # retorna la posicion del valor que busquemos en la tupla  este caso 3
+print(len(tupla)) # Muestra cuantos elementos existen en una tupla 
+print(tupla.count(8)) # Muestra cuantos valores se repiten en este caso cuantos 8 hay en la tupla
